188e91625943362be3b84fb5071b3d6922997c53
Felix <742863722@qq.com>
2020-04-18 21:52:52 +0800

    init project

---
+++ client/src/main/java/com/oocl/client/ClientApplication.java
@@ -0,0 +1,13 @@
+package com.oocl.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+}
