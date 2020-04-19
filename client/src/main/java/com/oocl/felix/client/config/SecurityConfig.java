89159f113a1497e33dd6f50c18c89abc3c4760eb
Felix <742863722@qq.com>
2020-04-19 20:29:31 +0800

    Updated client security config to make that any request need to authenticate to access

--- client/src/main/java/com/oocl/felix/client/config/SecurityConfig.java
+++ client/src/main/java/com/oocl/felix/client/config/SecurityConfig.java
@@ -31,7 +31,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/", "/index").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic()
